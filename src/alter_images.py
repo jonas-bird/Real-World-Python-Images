8bf9b0eb0fef15e045f9d73e0fd6e5b0d4716ff6
Jonas Bird <91971493+jonas-bird@users.noreply.github.com>
2022-01-12 16:57:04 -0500

    modify src/alter_images.py to complete lab

--- src/alter_images.py
+++ src/alter_images.py
@@ -1,35 +1,47 @@
-#!/usr/bin/env python3
-"""
-Code for Week 1 lab for Automating Real World tasks module from
-Google's IT Automation with Python
+!/usr/bin/env python3
 
-Jonas Bird
-2021-12-20
-"""
+# Solution to Google's IT Automation with Python: Automating Real World Tasks With Python: Week 1 Lab
+# Jonas Bird
+# 2021-12-23
 
 from pathlib import Path
 from PIL import Image
 
 
-def manipulate_images(fileNames):
-    """function that recieves a list of files and rotates, resizes,
-    and saves them in a format, and returns a list of the new filenames"""
+def manipulate_images(fileNames, destinationPath):
+    """
+    function that recieves a list of files, rotates and resizes them,
+    and saves them as .jpeg
+    """
+    size = (128, 128)
     newFiles = []
     for filename in fileNames:
-        image = Image.open(filename)
-        image.show()
-        newFiles.append(str(filename.with_suffix('.jpeg')))
+        try:
+            newFileName = Path(filename).stem + '.jpeg'
+            newFileName = Path(destinationPath) / newFileName
+            with Image.open(filename) as im:
+                im = im.convert("RGB")
+                im.thumbnail(size)
+                im = im.rotate(270, expand=True)
+                im.save(newFileName, "JPEG")
+        except Exception:
+            pass
+
+        newFiles.append(newFileName)
     return newFiles
 
-if __name__ == '__main__':
 
-    alteredFiles = []
-    # Prompt the user for a target directory, use a default if input is blank
-    userInput = input("Please enter the target directory:")
-    if len(userInput) < 1:
-        userInput = './images/'
-    # Trying out the newer pathlib rather than the os.path and globing
-    dirName = Path(userInput)
-    targetFiles = list(dirName.glob('*.ppm'))
-    alteredFiles = manipulate_images(targetFiles)
-    print(', '.join(alteredFiles))
+if __name__ == "__main__":
+    fileNames = []
+    path = Path(
+        '/home/student-04-3cd8df6fa80a/images/')
+    # must be full path unless I add expand home
+    destinationPath = '/opt/icons/'
+    # I had originally assumed that the files would have a .tiff extension and would be globbing them that way
+    for fNames in path.glob('*'):
+        fileNames.append(fNames)
+
+    newNames = manipulate_images(fileNames, destinationPath)
+    print("New thumbnails saved as: ")
+    for n in newNames:
+        print(n)
